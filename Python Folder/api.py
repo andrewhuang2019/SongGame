5b1a230e631afa98189d816d1febaf0fa0f66acd
Andysword <andrewhuang2019@gmail.com>
2023-12-21 15:21:21 -0600

    Added example code
    Added some of the example code from the documentation to the API class. Created environmental variables for the Spotify API to use.

--- Python Folder/api.py
+++ Python Folder/api.py
@@ -1,11 +1,17 @@
 # api.py
 
+import os
 import spotipy
 import requests
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 
 class API:
     def __init__(self):
+
+        os.environ['SPOTIPY_CLIENT_ID'] = '11fa54af84e7489eb6ceeea69ccd38d6'
+        os.environ['SPOTIPY_CLIENT_SECRET'] = '257c188fd54c4e9c86a964982f22bcc8'
+        os.environ['SPOTIPY_REDIRECT_URI'] = 'http://songgame.com'
         
         scope = "user-library-read"
         
@@ -16,5 +22,16 @@ class API:
         for idx, item in enumerate(results['items']):
             track = item['track']
             print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+        '''lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
+        spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+        results = spotify.artist_top_tracks(lz_uri)
+
+        for track in results['tracks'][:10]:
+            print('track    : ' + track['name'])
+            print('audio    : ' + track['preview_url'])
+            print('cover art: ' + track['album']['images'][0]['url'])
+            print()'''
     
     
