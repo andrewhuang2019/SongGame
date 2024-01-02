d488c45265e8e0857bd19b0b2ec32c631530b707
Andysword <andrewhuang2019@gmail.com>
2024-01-02 15:06:27 -0600

    did something with the comments

--- Python Folder/api.py
+++ Python Folder/api.py
@@ -23,23 +23,23 @@ class API:
         preview_urls = []
         random_songs = []
 
-        #oauth setup for our spotify account
+        # oauth setup for our spotify account
         cid = '11fa54af84e7489eb6ceeea69ccd38d6'
         secret = '257c188fd54c4e9c86a964982f22bcc8'
         client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
         sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-        #pulling playlist info 
+        # pulling playlist info
         playlist = sp.user_playlist_tracks('11fa54af84e7489eb6ceeea69ccd38d6','49se2DJsnGlPGrbXZgwQ5T')["items"]
         for track in playlist: 
             preview_urls.append(track['track']['preview_url'])
 
-        #randomly selecting 8 songs
+        # randomly selecting 8 songs
         index_list = random.sample(range(0, len(preview_urls)-1), 8)
         for index in index_list:
             random_songs.append(preview_urls[index])
 
-        #print out urls
+        # print out urls
         print()
         order = 1
         for song in random_songs:
