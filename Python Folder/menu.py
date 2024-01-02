c6cdf80907ae69b4692245c85465f346c527df9a
Andysword <andrewhuang2019@gmail.com>
2024-01-02 14:24:14 -0600

    Updated Game Flow
    Updated the logic inside of the game so that it asks players for the inputs (rating and guessing)

--- Python Folder/menu.py
+++ Python Folder/menu.py
@@ -12,17 +12,51 @@ class Menu:
         self.api.quick_start()
 
     def menu(self):
-        pass
+
+        while True:
+
+            rating1 = input("Enter rating of song #1: ")
+
+            rating2 = input("Enter rating of song #2: ")
+
+            rating3 = input("Enter rating of song #3: ")
+
+            rating4 = int(input("Enter rating of song #4: "))
+
+            guess = int(input("Enter guess of song #4: "))
+
+            player_one_score = self.point_calculator(rating4, guess)
+
+            rating1 = input("Enter rating of song #1: ")
+
+            rating2 = input("Enter rating of song #2: ")
+
+            rating3 = input("Enter rating of song #3: ")
+
+            rating4 = int(input("Enter rating of song #4: "))
+
+            guess = int(input("Enter guess of song #4: "))
+
+            player_two_score = self.point_calculator(rating4, guess)
+
+            if self.win_calculator(player_one_score, player_two_score) == "0":
+                print("Tie!")
+            elif self.win_calculator(player_one_score, player_two_score):
+                print("Player 1 Wins!")
+            else:
+                print("Player 2 Wins!")
 
     # ratings will be in .1 decimal form out of 10. 
     # closer to rating = higher score
     def point_calculator(self, rater_rating, guesser_rating):
         
-        #calculation that gives score based on how close from 10 to 1
+        # calculation that gives score based on how close from 10 to 1
         score = 10 - math.abs(rater_rating - guesser_rating)
         
         return score
 
-    def win_calculator(self):
-        pass
+    def win_calculator(self, game_score, other):
+        if game_score == other:
+            return "0"
+        return game_score > other
 
