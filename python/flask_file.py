1e4feeecbfae9c32468ea6a6c333ebb84a2a4b96
Andysword <andrewhuang2019@gmail.com>
2024-01-09 14:23:05 -0600

    Updated game_page
    added song urls data to the game page.

--- python/flask_file.py
+++ python/flask_file.py
@@ -1,6 +1,7 @@
 # flask_file.py
 
 from flask import Flask, render_template
+from api import API
 
 
 app = Flask(__name__)
@@ -8,11 +9,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template('mainpage.html')
+    return render_template('mainpage.html', data=[])
 
 @app.route('/game')
 def game_page():
-    return render_template('game.html')
+    song_api = API()
+    song_urls = song_api.get_playlist_urls('https://open.spotify.com/playlist/5VvixKeAd1Q2pjsxwG9b2X')
+    return render_template('game.html', song_urls=song_urls)
 
 
 
