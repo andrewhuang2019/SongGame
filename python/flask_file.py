01cb49e00165db037fc679ef15002af8802469e0
Andysword <andrewhuang2019@gmail.com>
2024-01-09 14:14:20 -0600

    Created 2 pages
    Made the main page and the game page. Used render template to run the html files and send them to the server.

--- python/flask_file.py
+++ python/flask_file.py
@@ -1,12 +1,18 @@
 # flask_file.py
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello(self):
-    return 'Hello World!'
+def main_page():
+    return render_template('mainpage.html')
+
+@app.route('/game')
+def game_page():
+    return render_template('game.html')
+
+
 
