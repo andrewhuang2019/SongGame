a32e00bba89de9a7e1f4be49a75af824da218b89
Cooper Wright <73917610+CooperW12@users.noreply.github.com>
2024-01-10 14:27:05 -0600

    functional?

--- python/static/scripts/mainpage.js
+++ python/static/scripts/mainpage.js
@@ -1,5 +1,5 @@
 document.getElementById("startButton").addEventListener("click", redirectToGame)
 
 function redirectToGame() {
-    location.href = "../pages/game.html"
+    location.href = "/game"
 }
