b82630720e5abfd4afbe767237e56f5ddf234213
Andysword <andrewhuang2019@gmail.com>
2024-01-12 19:46:21 -0600

    Fixed URL errors
    Fixed the error which didn't allow other playlist links to be input into the website.

--- python/api.py
+++ python/api.py
@@ -31,14 +31,14 @@ class API:
     
     def get_playlist_information(self, playlist_link):
 
-        playlist_link = playlist_link.split('/')
+        #playlist_link = playlist_link.split('/')
 
         cid = '11fa54af84e7489eb6ceeea69ccd38d6'
         secret = '257c188fd54c4e9c86a964982f22bcc8'
         client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
         sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-        playlist = sp.user_playlist('11fa54af84e7489eb6ceeea69ccd38d6', playlist_link[-1])
+        playlist = sp.user_playlist('11fa54af84e7489eb6ceeea69ccd38d6', playlist_link)
 
         playlist_name = playlist['name']
 
@@ -50,7 +50,7 @@ class API:
     def get_playlist_urls(self,playlist_link):
         tracks = []
         random_songs = []
-        playlist_link = playlist_link.split('/')
+        #playlist_link = playlist_link.split('/')
 
         # oauth setup for our spotify account
         cid = '11fa54af84e7489eb6ceeea69ccd38d6'
@@ -59,7 +59,10 @@ class API:
         sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
         # pulling playlist info
-        playlist = sp.user_playlist_tracks('11fa54af84e7489eb6ceeea69ccd38d6', playlist_link[-1])["items"]
+        #playlist = sp.user_playlist_tracks('11fa54af84e7489eb6ceeea69ccd38d6', playlist_link[-1])["items"]
+
+        playlist = sp.user_playlist_tracks('11fa54af84e7489eb6ceeea69ccd38d6', playlist_link)['items']
+
         for track in playlist: 
 
             preview_url = track['track']['preview_url']
--- python/main.py
+++ python/main.py
@@ -10,8 +10,8 @@ menu = Menu()
 #menu.run()
 api = API()
 
-#api.get_playlist_urls('https://open.spotify.com/playlist/5VvixKeAd1Q2pjsxwG9b2X')
-api.get_playlist_information('https://open.spotify.com/playlist/5VvixKeAd1Q2pjsxwG9b2X')
+api.get_playlist_urls('https://open.spotify.com/playlist/5VvixKeAd1Q2pjsxwG9b2X')
+#api.get_playlist_information('https://open.spotify.com/playlist/5VvixKeAd1Q2pjsxwG9b2X')
 
 def testing():
     pass
